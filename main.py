eecffcd2cf75a1a28f75fd12dabc3b5d0d931183
wangukwa <zacknazareth0@gmail.com>
2019-02-13 18:41:58 +0300

    json data

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import datetime
 import smtplib
 import time
+from string import Template
+import json
 
-cnts = []
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+cnts = {}
+msg = []
 
 class Scheduler(object):
     def __init__(self, contacts, message):
@@ -10,26 +16,32 @@ class Scheduler(object):
         self.message =  message
 
     def get_contacts(self):
-        file = open(self.contacts, 'r')
-        for contact in file:
-            cnts.append(contact)
+        cnts_data = open(self.contacts, 'r', encoding="utf-8")
+        json_data = json.load(cnts_data)
+        for key in json_data:
+            cnts[key] = json_data[key]
 
     def get_message(self):
         with open(self.message, 'r', encoding='utf-8') as template_file:
             template_file_content = template_file.read()
-        return Template(template_file_content)
+        msg.append(template_file_content)
 
 def main():
-    email_scheduler = Scheduler('contacts.txt', 'message.txt')
-    email_scheduler.get_contacts()
+    MY_ADDRESS ="anwar@tunapanda.org"
+    PASSWORD = ""
+    # s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    # s.starttls()
+    # s.login(MY_ADDRESS, PASSWORD)
 
-    print(email_scheduler.get_message())
+    email_scheduler = Scheduler('contacts.json', 'message.txt')
+    email_scheduler.get_contacts()
+    message = email_scheduler.get_message()
+
+    print(cnts)
 
     send_date = datetime.datetime(2019, 2, 13, 17, 45)
     tm = datetime.datetime.now()
     print(tm.time(), send_date.time())
-    if tm.time() == send_date.time():
-        print("time")
 
 if __name__ == '__main__':
     main()
